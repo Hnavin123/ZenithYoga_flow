c4bf2b9b9e65419bde72bb27710504bfed730529
amar2998 <amarsonunath1998@gmail.com>
2025-06-10 13:27:11 +0530

    App.js is now towrikng

--- src/App.js
+++ src/App.js
@@ -19,97 +19,40 @@
 
 // export default App;
 
-<<<<<<< Updated upstream
-// import React from "react";
-// import Description from "./Description/Description";
-// import Footer from "./Footer/Footer";
-// import "./App.css";
-// import Header from "./Header/Header";
-// import SideBar from "./SideBar/SideBar";
-// import Rout from "./Routing/Routing";
-// // import CartProvider from "./CartContext";
-// import { CartProvider } from "./CartContext"; // ✅ Correct
-
-
-// const App = () => {
-//   return (
-//     <>
-//       <div className="header">
-//         <Header />
-//       </div>
-//       <div style={{ display: "flex" }}>
-//         {/* <div className='main-body'> */}
-//         <div className="sideBar">
-//           <SideBar />
-//         </div>
-//         <div className="main-content">
-//           <div>
-//             <Rout />
-//           </div>
-//           <div className="footer">
-//             <Footer />
-//           </div>
-//         </div>
-//         {/* </div> */}
-//       </div>
-//     </>
-//   );
-// };
-
-// export default App;
-
-
-
-
-
-
 import React from "react";
 import Description from "./Description/Description";
 import Footer from "./Footer/Footer";
-import "./App.css";
 import Header from "./Header/Header";
-import SideBar from "./SideBar/SideBar";
-import Rout from "./Routing/Routing";
-import { CartProvider } from "./CartContext"; // ✅ Correct
-
-const App = () => {
-  return (
-    <CartProvider>
-      <div className="header">
-        <Header />
-      </div>
-      <div style={{ display: "flex" }}>
-        <div className="sideBar">
-          <SideBar />
-=======
-import React from 'react';
-import Description from './Description/Description';
-import Footer from './Footer/Footer';
-import Header from './Header/Header';
-import Sidebar from './SIdebar/Sidebar';
-import './App.css';
+import Sidebar from "./SideBar/SideBar";
+import { Route } from "react-router";
+import "./App.css";
 
 const App = () => {
   return (
     <>
-      <div className='header'><Header /></div>
-      <div style={{ display: 'flex' }}>
-        <div className='main-body'>
-        <div className="menu"><Sidebar /></div>
-        <div className='main'>
-          <Description />
->>>>>>> Stashed changes
-        </div>
-        <div className="main-content">
-          <div>
-            <Rout />
-          </div>
-          <div className="footer">
-            <Footer />
-          </div>
-        </div>
+      <div className="header">
+        <Header />
       </div>
-    </CartProvider>
+      <div style={{ display: "flex" }}>
+        <div className="main-body">
+          <div className="menu">
+            <Sidebar />
+          </div>
+          <div className="main">
+            <Description />
+          </div>
+          <div className="main-content">
+            <div>
+              <Route />
+            </div>
+            <div className="footer">
+              <Footer />
+            </div>
+          </div>
+        </div>
+        {/* </CartProvider> */}
+      </div>
+    </>
   );
 };
 
